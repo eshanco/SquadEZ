77d1b680dba5e1b0a006895bcc3cd997ff7b8ed4
eshanco <shane.connaughton@ericsson.com>
2026-08-28 15:14:05 +0100

    Drop jersey number from lineup pitch chips, keep it on the bench

--- src/components/lineup/PlayerChip.tsx
+++ src/components/lineup/PlayerChip.tsx
@@ -24,7 +24,7 @@ export function PlayerChip({
         isDragging ? 'opacity-40' : ''
       } ${compact ? 'w-full text-center' : ''}`}
     >
-      #{player.jerseyNumber} {player.firstName}
+      {compact ? player.firstName : `#${player.jerseyNumber} ${player.firstName}`}
     </div>
   )
 }
